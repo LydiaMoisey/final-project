83701a3f01acbce621226187e57f1081782d8a73
LydiaMoisey <122109993+LydiaMoisey@users.noreply.github.com>
2023-04-12 23:14:21 +0300

    Update User.java

--- src/main/java/by/itacademy/moiseenkolydia/breezy/ui/domain/User.java
+++ src/main/java/by/itacademy/moiseenkolydia/breezy/ui/domain/User.java
@@ -4,15 +4,8 @@ import com.github.javafaker.Faker;
 
 public class User {
     static Faker faker = new Faker();
-    private String email;
-    private String password;
     public static final String VALID_EMAIL = "99camomile99@gmail.com";
     public static final String VALID_PASSWORD = "Qwerty!1";
     public static final String RANDOM_EMAIL = faker.internet().emailAddress();
     public static final String RANDOM_PASSWORD = faker.internet().password(6, 14);
-
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
