28b4274da8454c687e5f24ce3337f07ac40f6749
LydiaMoisey <122109993+LydiaMoisey@users.noreply.github.com>
2023-04-14 17:16:40 +0300

    refactor TestData

--- src/test/java/by/itacademy/moiseenkolydia/breezy/api/TestData.java
+++ src/test/java/by/itacademy/moiseenkolydia/breezy/api/TestData.java
@@ -6,12 +6,10 @@ public class TestData {
     static Faker faker = new Faker();
     public static final String BASE_URL = "https://breezy.by";
     public static final String LOGIN_URL = "https://breezy.by/profile/login";
-    public static final String SEARCH_URL = "https://breezy.by/search";
     public static final String VALID_EMAIL = "99camomile99@gmail.com";
     public static final String VALID_PASSWORD = "Qwerty!1";
     public static final String RANDOM_EMAIL = faker.internet().emailAddress();
     public static final String RANDOM_PASSWORD = faker.internet().password(6, 14);
-    public static final String TEXT_SEARCH = "наушники";
     public static final String ERROR_INVALID_EMAIL_AND_PASSWORD = "Неправильный логин или пароль";
     public final static String ERROR_WITHOUT_EMAIL =
             "Форма содержит ошибки: <p>- Email -- Обязательное поле<br>";
